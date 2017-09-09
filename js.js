d7aa81b224c21f9f952f7dc7059e10163fbf4d40
AlbertoVsq <alberto.avm2010@gmail.com>
2017-09-09 00:36:27 -0600

    primer commit

---
+++ js.js
@@ -0,0 +1,9 @@
+$(document).ready(inicio)
+function inicio(){
+
+	$(".botoncompra").click(agregar)
+	$("#carrito").load("poncarrito.php");
+}
+function agregar(){
+	$("#carrito").load("poncarrito.php?p="+$(this).val());
+}
